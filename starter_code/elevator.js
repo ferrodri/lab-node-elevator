76b81570030a75d7169eef1d8c5a3045c8252204
ferrodri <f.rodriguez.hervias@gmail.com>
2017-07-03 19:57:40 +0200

    on process

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,70 @@
+/*jshint esversion: 6*/
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.waitingList = [];
+    this.passengers = [];
+    this.requests = [];
+    this.direction = "UP";
+    this.intervalId = "";
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  start(person) {
+    this.call(person);
+    this.intervalId = setInterval(() => {
+      this._passengersEnter();
+     }, 1000);
+  }
+  stop() {
+    clearInterval(this.intervalId);
+  }
+  update() {
+    this.log();
+  }
+  _passengersEnter() {
+    for ( let i= this.waitingList.length; i > 0; i--) {
+      if (this.waitingList[0].originFloor === this.floor) {
+        this.passengers.push(this.waitingList[0]);
+        this.requests.push(this.waitingList[0]);
+        this.waitingList.splice(0, 1);
+        console.log(this.passengers);
+        this.floorUp();
+      } else {
+        this.floorUp();
+      }
+    }
+  }
+  _passengersLeave() {}
+
+  floorUp() {
+    if (this.floor < this.MAXFLOOR) {
+      this.direction = "UP";
+      this.floor++;
+      this.update();
+    } else {
+      console.log("Maximum floor reached!");
+      this.floorDown();
+    }
+  }
+  floorDown() {
+    // while (this.requests.length > 0) {
+      if (this.floor > 0) {
+        this.direction = "DOWN";
+        this.floor--;
+        this.update();
+      } else {
+        console.log("Minimum floor reached!");
+        this.floorUp();
+      }
+    // }
+  }
+  call(person) {
+    this.waitingList.push(person);
+  }
+  log() {
+    console.log("Floor: " + this.floor + " | Direction: " + this.direction);
+  }
 }
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,10 @@
+/*jshint esversion: 6*/
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+const person = new Person("fer", 3, 10);
+const elevator = new Elevator();
+
+
+elevator.start(person);
